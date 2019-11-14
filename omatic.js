7d64a25a0c4bd3a32492a6c8566ef575c6bc8f1f
sangita <42108271+sangita05@users.noreply.github.com>
2019-11-14 15:19:31 +1300

    Omatic
    wow!!!

---
+++ omatic.js
@@ -0,0 +1,39 @@
+// var user = prompt("are you there yet?");
+// while(user.indexof() === -1 )
+// {
+// 	var user = prompt("are you there yet?");
+// }
+// alert("yeyyy, we made it");
+
+for(var i=0; i<16; i+=8)
+{
+	console.log(i);
+}
+var str = "ahceclwlxo";
+for(var i=1; i<str.length; i+=2){
+	console.log(str[i]);
+}
+console.log("--------------------------------------");
+
+for(var i=-10; i<=19; i++)
+{
+	console.log(i);
+}
+console.log("--------------------------------------");
+for(var i=10; i<=40; i+=2)
+{
+console.log(i);
+}
+console.log("--------------------------------------");
+for(var i=301; i<=333; i+=2)
+{
+console.log(i);
+}
+console.log("--------------------------------------");
+for(var i=5; i<=50; i++)
+{
+	if(i%5===0 && i%3===0){
+	console.log(i);	
+	}
+
+}
